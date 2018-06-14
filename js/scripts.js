7d8b6952f6d863b2f3eff9ffafe680f5218dee63
cb <dragonflylord@gmail.com>
2018-06-13 22:20:52 -0700

    it will now work with 2 players

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,15 @@
   this.player1 = player1;
   this.player2 = player2;
 }*/
+
+var turn =0
+var currentTurn = 0
 var newTotal = new Total();
 var newHold = new Current();
+var player2hold = new Current();
+var player2total = new Total();
 var currentHold = 0
+
 var dice = {
   sides: 6,
   roll: function () {
@@ -14,6 +20,7 @@ var dice = {
     +1;
     if (randomNumber === 1){
       currentHold = 0
+
     }
     else {
       currentHold = currentHold + randomNumber;
@@ -23,19 +30,25 @@ var dice = {
 }
 function Total() {
   this.current = 0;
+
 }
-function Total1() {
-  this.current = 0;
-}
+
+
 function Current() {
   this.currentHold = 0;
 }
 Total.prototype.totalHold = function(){
   this.current += currentHold;
+  turn++
+  turnTracker()
 }
-Total.prototype.clone = function(){
-  this.current += currentHold;
+
+function turnTracker(){
+  if(turn % 2 === 0){
+    currentTurn ++
+  }
 }
+
 
 
 //user interface
@@ -43,8 +56,13 @@ $(document).ready(function(){
 //player1
   $("form#lucky").submit(function(event) {
     event.preventDefault();
+
     $("#rolls").text(dice.roll());
     $("#hold").text(currentHold);
+    if(currentHold ===0){
+      $("form#form-hold").submit();
+    }
+    $("#turnCounter").text(currentTurn)
   });
 
   $("form#form-hold").submit(function(event) {
@@ -53,25 +71,36 @@ $(document).ready(function(){
     $("#total").text(newTotal.current);
     currentHold = 0;
     $("#hold").text(currentHold);
+    $("#turnCounter").text(currentTurn)
+    $("form#form-hold").toggle();
+    $("form#form-hold1").toggle();
+    $("form#lucky").toggle();
+    $("form#lucky1").toggle();
   });
 //player2
   $("form#lucky1").submit(function(event) {
     event.preventDefault();
     $("#rolls1").text(dice.roll());
     $("#hold1").text(currentHold);
+    if(currentHold ===0){
+      $("form#form-hold1").submit();
+    }
+    $("#turnCounter").text(currentTurn)
   });
-
+ $("")
   $("form#form-hold1").submit(function(event) {
     event.preventDefault();
-    newTotal1.clone();
-    $("#total1").text(newTotal1.current);
+    player2total.totalHold();
+    $("#total1").text(player2total.current);
     currentHold = 0;
     $("#hold1").text(currentHold);
+    $("#turnCounter").text(currentTurn)
+    $("form#form-hold").toggle();
+    $("form#form-hold1").toggle();
+    $("form#lucky").toggle();
+    $("form#lucky1").toggle();
   });
 
 
   $("#winner").show();
   $("#winner").text()
-
-
-});
